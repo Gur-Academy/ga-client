6276de33eb8a4e2de395bbc7b4221a47153848bd
codebyjsa <jsaishere@gmail.com>
2025-06-16 11:32:54 +0530

    setup

---
+++ src/main/java/com/guracademy/GaClient/GaClientApplication.java
@@ -0,0 +1,13 @@
+package com.guracademy.GaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guracademy/GaClient/GaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guracademy.GaClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
